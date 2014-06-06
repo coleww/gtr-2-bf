02d7e5669bf6713d36bd030a3cfec8c8b9c3e36d
Cole Willsea <coleww@gmail.com>
2014-06-05 19:37:38 -0700

    LETS SEE IT YA:LL

--- js/pitchdetect.js
+++ js/pitchdetect.js
@@ -29,12 +29,7 @@ var isPlaying = false;
 var sourceNode = null;
 var analyser = null;
 var theBuffer = null;
-var detectorElem, 
-	canvasContext,
-	pitchElem,
-	noteElem,
-	detuneElem,
-	detuneAmount;
+var noteElem;
 var WIDTH=300;
 var CENTER=150;
 var HEIGHT=42;
@@ -42,10 +37,10 @@ var confidence = 0;
 var currentPitch = 0;
 
 window.onload = function() {
-	toggleLiveInput()
-	noteElem = document.getElementById( "note" );
-	canvasContext = document.getElementById( "output" ).getContext("2d");
-}
+  getLiveInput();
+  noteElem = document.getElementById( "note" );
+  canvasContext = document.getElementById( "output" ).getContext("2d");
+};
 
 function error() {
     alert('Stream generation failed.');
@@ -53,10 +48,7 @@ function error() {
 
 function getUserMedia(dictionary, callback) {
     try {
-        navigator.getUserMedia = 
-        	navigator.getUserMedia ||
-        	navigator.webkitGetUserMedia ||
-        	navigator.mozGetUserMedia;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
         navigator.getUserMedia(dictionary, callback, error);
     } catch (e) {
         alert('getUserMedia threw exception :' + e);
@@ -71,272 +63,89 @@ function gotStream(stream) {
     analyser = audioContext.createAnalyser();
     analyser.fftSize = 2048;
     mediaStreamSource.connect( analyser );
-    updatePitch();
+    window.setInterval(function(){
+      updatePitch();
+    }, 500);
 }
 
-function toggleLiveInput() {
+function getLiveInput() {
     getUserMedia({audio:true}, gotStream);
 }
 
-function togglePlayback() {
-    var now = audioContext.currentTime;
-
-    if (isPlaying) {
-        //stop playing and return
-        sourceNode.stop( now );
-        sourceNode = null;
-        analyser = null;
-        isPlaying = false;
-		if (!window.cancelAnimationFrame)
-			window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
-        window.cancelAnimationFrame( rafID );
-        return "start";
-    }
-
-    sourceNode = audioContext.createBufferSource();
-    sourceNode.buffer = theBuffer;
-    sourceNode.loop = true;
-
-    analyser = audioContext.createAnalyser();
-    analyser.fftSize = 2048;
-    sourceNode.connect( analyser );
-    analyser.connect( audioContext.destination );
-    sourceNode.start( now );
-    isPlaying = true;
-    isLiveInput = false;
-    updatePitch();
-
-    return "stop";
-}
-
-var rafID = null;
 var tracks = null;
 var buflen = 2048;
 var buf = new Uint8Array( buflen );
 var MINVAL = 134;  // 128 == zero.  MINVAL is the "minimum detected signal" level.
 
-/*
-function findNextPositiveZeroCrossing( start ) {
-	var i = Math.ceil( start );
-	var last_zero = -1;
-	// advance until we're zero or negative
-	while (i<buflen && (buf[i] > 128 ) )
-		i++;
-	if (i>=buflen)
-		return -1;
-
-	// advance until we're above MINVAL, keeping track of last zero.
-	while (i<buflen && ((t=buf[i]) < MINVAL )) {
-		if (t >= 128) {
-			if (last_zero == -1)
-				last_zero = i;
-		} else
-			last_zero = -1;
-		i++;
-	}
-
-	// we may have jumped over MINVAL in one sample.
-	if (last_zero == -1)
-		last_zero = i;
-
-	if (i==buflen)	// We didn't find any more positive zero crossings
-		return -1;
-
-	// The first sample might be a zero.  If so, return it.
-	if (last_zero == 0)
-		return 0;
-
-	// Otherwise, the zero might be between two values, so we need to scale it.
-
-	var t = ( 128 - buf[last_zero-1] ) / (buf[last_zero] - buf[last_zero-1]);
-	return last_zero+t;
-}
-*/
-
 var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
 function noteFromPitch( frequency ) {
-	var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
-	return Math.round( noteNum ) + 69;
+  var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
+  return Math.round( noteNum ) + 69;
 }
 
 function frequencyFromNoteNumber( note ) {
-	return 440 * Math.pow(2,(note-69)/12);
+  return 440 * Math.pow(2,(note-69)/12);
 }
 
 function centsOffFromPitch( frequency, note ) {
-	return ( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
+  return ( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
 }
-
-// this is a float version of the algorithm below - but it's not currently used.
-/*
-function autoCorrelateFloat( buf, sampleRate ) {
-	var MIN_SAMPLES = 4;	// corresponds to an 11kHz signal
-	var MAX_SAMPLES = 1000; // corresponds to a 44Hz signal
-	var SIZE = 1000;
-	var best_offset = -1;
-	var best_correlation = 0;
-	var rms = 0;
-
-	if (buf.length < (SIZE + MAX_SAMPLES - MIN_SAMPLES))
-		return -1;  // Not enough data
-
-	for (var i=0;i<SIZE;i++)
-		rms += buf[i]*buf[i];
-	rms = Math.sqrt(rms/SIZE);
-
-	for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset++) {
-		var correlation = 0;
-
-		for (var i=0; i<SIZE; i++) {
-			correlation += Math.abs(buf[i]-buf[i+offset]);
-		}
-		correlation = 1 - (correlation/SIZE);
-		if (correlation > best_correlation) {
-			best_correlation = correlation;
-			best_offset = offset;
-		}
-	}
-	if ((rms>0.1)&&(best_correlation > 0.1)) {
-		console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")");
-	}
-//	var best_frequency = sampleRate/best_offset;
-}
-*/
 
 function autoCorrelate( buf, sampleRate ) {
-	var MIN_SAMPLES = 4;	// corresponds to an 11kHz signal
-	var MAX_SAMPLES = 1000; // corresponds to a 44Hz signal
-	var SIZE = 1000;
-	var best_offset = -1;
-	var best_correlation = 0;
-	var rms = 0;
+  var MIN_SAMPLES = 4;  // corresponds to an 11kHz signal
+  var MAX_SAMPLES = 1000; // corresponds to a 44Hz signal
+  var SIZE = 1000;
+  var best_offset = -1;
+  var best_correlation = 0;
+  var rms = 0;
 
-	confidence = 0;
-	currentPitch = 0;
+  confidence = 0;
+  currentPitch = 0;
 
-	if (buf.length < (SIZE + MAX_SAMPLES - MIN_SAMPLES))
-		return;  // Not enough data
+  if (buf.length < (SIZE + MAX_SAMPLES - MIN_SAMPLES))
+    return;  // Not enough data
 
-	for (var i=0;i<SIZE;i++) {
-		var val = (buf[i] - 128)/128;
-		rms += val*val;
-	}
-	rms = Math.sqrt(rms/SIZE);
+  for (var i=0;i<SIZE;i++) {
+    var val = (buf[i] - 128)/128;
+    rms += val*val;
+  }
+  rms = Math.sqrt(rms/SIZE);
 
-	for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset++) {
-		var correlation = 0;
+  for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset++) {
+    var correlation = 0;
 
-		for (var i=0; i<SIZE; i++) {
-			correlation += Math.abs(((buf[i] - 128)/128)-((buf[i+offset] - 128)/128));
-		}
-		correlation = 1 - (correlation/SIZE);
-		if (correlation > best_correlation) {
-			best_correlation = correlation;
-			best_offset = offset;
-		}
-	}
-	if ((rms>0.01)&&(best_correlation > 0.01)) {
-		confidence = best_correlation * rms * 10000;
-		currentPitch = sampleRate/best_offset;
-		// console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
-	}
-//	var best_frequency = sampleRate/best_offset;
+    for (var i=0; i<SIZE; i++) {
+      correlation += Math.abs(((buf[i] - 128)/128)-((buf[i+offset] - 128)/128));
+    }
+    correlation = 1 - (correlation/SIZE);
+    if (correlation > best_correlation) {
+      best_correlation = correlation;
+      best_offset = offset;
+    }
+  }
+  if ((rms>0.01)&&(best_correlation > 0.01)) {
+    confidence = best_correlation * rms * 10000;
+    currentPitch = sampleRate/best_offset;
+    // console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
+  }
+//  var best_frequency = sampleRate/best_offset;
 }
 
-function updatePitch( time ) {
-	var cycles = new Array;
-	analyser.getByteTimeDomainData( buf );
+function updatePitch() {
+  var cycles = new Array;
+  analyser.getByteTimeDomainData( buf );
 
-/*
-// old zero-crossing code
+  // possible other approach to confidence: sort the array, take the median; go through the array and compute the average deviation
+  autoCorrelate( buf, audioContext.sampleRate );
 
-	var i=0;
-	// find the first point
-	var last_zero = findNextPositiveZeroCrossing( 0 );
+  canvasContext.fillStyle = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+  canvasContext.clearRect(0,0,WIDTH,HEIGHT);
 
-	var n=0;
-	// keep finding points, adding cycle lengths to array
-	while ( last_zero != -1) {
-		var next_zero = findNextPositiveZeroCrossing( last_zero + 1 );
-		if (next_zero > -1)
-			cycles.push( next_zero - last_zero );
-		last_zero = next_zero;
-
-		n++;
-		if (n>1000)
-			break;
-	}
-
-	// 1?: average the array
-	var num_cycles = cycles.length;
-	var sum = 0;
-	var pitch = 0;
-
-	for (var i=0; i<num_cycles; i++) {
-		sum += cycles[i];
-	}
-
-	if (num_cycles) {
-		sum /= num_cycles;
-		pitch = audioContext.sampleRate/sum;
-	}
-
-// confidence = num_cycles / num_possible_cycles = num_cycles / (audioContext.sampleRate/)
-	var confidence = (num_cycles ? ((num_cycles/(pitch * buflen / audioContext.sampleRate)) * 100) : 0);
-*/
-
-/*
-	console.log( 
-		"Cycles: " + num_cycles + 
-		" - average length: " + sum + 
-		" - pitch: " + pitch + "Hz " +
-		" - note: " + noteFromPitch( pitch ) +
-		" - confidence: " + confidence + "% "
-		);
-*/
-	// possible other approach to confidence: sort the array, take the median; go through the array and compute the average deviation
-	autoCorrelate( buf, audioContext.sampleRate );
-
-// 	detectorElem.className = (confidence>50)?"confident":"vague";
-
-	canvasContext.clearRect(0,0,WIDTH,HEIGHT);
-
- 	if (confidence <10) {
- 		detectorElem.className = "vague";
-	 	pitchElem.innerText = "--";
-		noteElem.innerText = "-";
-		detuneElem.className = "";
-		detuneAmount.innerText = "--";
- 	} else {
-	 	detectorElem.className = "confident";
-	 	pitchElem.innerText = Math.floor( currentPitch ) ;
-	 	var note =  noteFromPitch( currentPitch );
-		noteElem.innerHTML = noteStrings[note%12];
-		var detune = centsOffFromPitch( currentPitch, note );
-		if (detune == 0 ) {
-			detuneElem.className = "";
-			detuneAmount.innerHTML = "--";
-
-			// TODO: draw a line.
-		} else {
-			if (Math.abs(detune)<10)
-				canvasContext.fillStyle = "green";
-			else
-				canvasContext.fillStyle = "red";
-
-			if (detune < 0) {
-	  			detuneElem.className = "flat";
-			}
-			else {
-				detuneElem.className = "sharp";
-			}
-  			canvasContext.fillRect(CENTER, 0, (detune*3), HEIGHT);
-			detuneAmount.innerHTML = Math.abs( Math.floor( detune ) );
-		}
-	}
-
-	if (!window.requestAnimationFrame)
-		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
-	rafID = window.requestAnimationFrame( updatePitch );
+  if (confidence <10) {
+    noteElem.innerText = "-";
+  } else {
+    var note =  noteFromPitch( currentPitch );
+    noteElem.innerHTML = noteStrings[note%12];
+  }
 }
